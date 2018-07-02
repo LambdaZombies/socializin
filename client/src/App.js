a3138d7f3a5e7ca7562244a503f1e0300fe5d5b9
Travis Jones <travisj_jones@hotmail.com>
2018-07-02 14:04:20 -0500

    Set up landing boiler & navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,38 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./App.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './Components/navbar';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Socializin</h1>
-                <p className="lead">
-                  {" "}
-                  Connect with your friends through their busy schedules.
-                </p>
-                <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
+      <div className="App">
+        <Navbar />
+        <div className="landing">
+          <div className="dark-overlay landing-inner text-light">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">Socializin</h1>
+                  <p className="lead">
+                    {' '}
+                    Connect with your friends through their busy schedules.
+                  </p>
+                  <hr />
+                  <Link to="/" className="btn btn-lg btn-info mr-2">
+                    Sign Up
+                  </Link>
+                  <Link to="/" className="btn btn-lg btn-light">
+                    Login
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
