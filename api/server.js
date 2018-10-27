3ea5f23dcf1273a99f2067fb858ad3fda55a4274
MannieJay <jhall02258@gmail.com>
2018-10-26 23:46:08 -0400

    reverted urls for deploy

--- api/server.js
+++ api/server.js
@@ -22,8 +22,8 @@ const db = require("../config/keys").mongoURI;
 
 mongoose.Promise = global.Promise;
 mongoose
-  // .connect("mongodb://Testy:abc123@ds125031.mlab.com:25031/socializin")
-  .connect("mongodb://localhost:27017/socializin")
+  .connect("mongodb://Testy:abc123@ds125031.mlab.com:25031/socializin")
+  // .connect("mongodb://localhost:27017/socializin")
   .then(function(db) {
     console.log("All your dbs are belong to us!");
     server.listen(port, function() {
--- client/src/Components/base.js
+++ client/src/Components/base.js
@@ -1,5 +1,5 @@
 // local development
-//const base = "https://socializin.herokuapp.com";
-const base = "http://localhost:3030";
+const base = "https://socializin.herokuapp.com";
+// const base = "http://localhost:3030";
 
 export { base as default };
